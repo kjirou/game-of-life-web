788aefbf36830acb39c558ba1918de463d360d5b
kjirou <sorenariblog@gmail.com>
2018-06-28 16:58:36 +0900

    Apply CSS

--- client/src/components/Root/index.js
+++ client/src/components/Root/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import styles from './style.scss';
+
 export default class Root extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         ROOT!!
       </div>
     );
