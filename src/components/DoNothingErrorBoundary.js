5717cfbc6c80ed2c170156d9ab0d059c283f9d56
kjirou <sorenariblog@gmail.com>
2018-06-29 14:32:25 +0900

    Delete flowtype definitions

--- src/components/DoNothingErrorBoundary.js
+++ src/components/DoNothingErrorBoundary.js
@@ -1,20 +1,7 @@
-// @flow
-
 import * as React from 'react';
 
-type Props = {
-  children: React.Node,
-};
-
-/**
- * 何もしないというエラーハンドリングの設定
- *
- * エラーハンドリングの設定を全くしない場合、コンポーネントのコンストラクタで例外が発生した際に
- * デフォルトの挙動が recreate を試みてしまい、結果として二重にエラーが出力されてしまう。
- * その対策として作成したもの。
- */
-export default class DoNothingErrorBoundary extends React.Component<Props> {
-  constructor(props: Props) {
+export default class DoNothingErrorBoundary extends React.Component {
+  constructor(props) {
     super(props);
   }
 
